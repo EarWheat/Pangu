04a544f61e3412412592fbdb21326ac92c005a44
EarWheat <2636965138@qq.com>
2020-03-16 18:45:12 +0800

    时间区间

--- src/main/java/com/pangu/time/TimeUtils.java
+++ src/main/java/com/pangu/time/TimeUtils.java
@@ -1,7 +1,12 @@
 package com.pangu.time;
 
-import java.util.Calendar;
-import java.util.Date;
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONArray;
+import com.sun.xml.internal.fastinfoset.util.StringArray;
+import org.springframework.util.StringUtils;
+
+import java.text.ParseException;
+import java.util.*;
 
 /*
  * @author:liuzhaolu
@@ -11,10 +16,16 @@ import java.util.Date;
 public class TimeUtils {
     private static final Long Hour = 60L * 60L * 1000;
 
-    /**获取当前时间的整点小时时间
+    public static void main(String[] args) {
+        System.out.println(timeArrayList("2019-09-16 15:02:30", "2020-03-16 07:01:35"));
+    }
+
+    /**
+     * 获取当前时间的整点小时时间
+     *
      * @return
      */
-    public Long getNextHourTime(){
+    public Long getNextHourTime() {
         Calendar ca = Calendar.getInstance();
         ca.set(Calendar.MINUTE, 0);
         ca.set(Calendar.SECOND, 0);
@@ -24,10 +35,21 @@ public class TimeUtils {
 
     /**
      * 获取当前毫秒级时间
+     *
      * @return
      */
-    public Long getCurrTime(){
+    public Long getCurrTime() {
         Date date = new Date();
         return date.getTime();
+    }
+
+    public static String timeArrayList(String s1, String s2) {
+        if (StringUtils.isEmpty(s1) || StringUtils.isEmpty(s2)) {
+            return null;
+        }
+        JSONArray jsonArray = new JSONArray();
+        jsonArray.add(s1);
+        jsonArray.add(s2);
+        return jsonArray.toString();
     }
 }
